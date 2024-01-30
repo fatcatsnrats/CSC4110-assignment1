4decc8077ebd2ba8239febf4d376cb86f92bb765
Omar Almasri <omaralmasri100@gmail.com>
2024-01-30 16:52:27 -0500

    Adding author name

--- HelloWorld-1.cpp
+++ HelloWorld-1.cpp
@@ -1,5 +1,5 @@
 // Simple C++ program to display "Hello World" 
-// Header file for input output functions 
+// Author: Omar Almasri <gw7523@wayne.edu>
 #include<iostream>  
   
 using namespace std; 
